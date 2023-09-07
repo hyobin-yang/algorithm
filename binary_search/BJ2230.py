7f0cc930f299a9da2d64d67b257d325abfff086a
hyobin-yang <hyobin3026@naver.com>
2023-09-07 16:01:21 +0900

    two pointer

--- binary_search/BJ2230.py
+++ binary_search/BJ2230.py
@@ -1,19 +1,10 @@
 ### 수 고르기
 
-# 오름차순 정렬
-# 이진탐색
-# 차이가 가장 작은 경우의 차이 출력
-# 차이가 가장 큰 것(마지막 요소 - 첫 요소)를 기본값으로 설정
-# 차이가 M 이상이어야 함
-# 차이 = abs(뺄셈)
-
-
-# 예제 맞음
-# 반례 존재
-
-n, m = map(int, input().split())
-number_list = [int(input()) for _ in range(n)]
-number_list.sort()
+# 두 수의 차이가 m 이상이면서 가장 작게
+# 차이 출력
+# m 이상일 때 차이를 더 작게
+# m 미만일 때 차이를 더 크게
+# 즉 차이를 좁힐 뿐만 아니라 넓히는 경우도 생각 -> two pointer 방법
 
 
 n, m = map(int, input().split())
@@ -21,28 +12,27 @@ number_list = [int(input()) for _ in range(n)]
 number_list.sort()
 
 
-def binary_search(arr, m):
-    tmp = abs(arr[-1] - arr[0])
-    left, right = 0, n-1
+def two_pointer(left, right):
+    ans = float('inf')
 
-    while left <= right:
-        minus = abs(arr[left] - arr[right])
-
-        if (minus >= m) and (minus < tmp):
-            tmp = minus
+    while left < n and right < n:
+        diff = number_list[right] - number_list[left]
+        # m보다 차이가 작으면 차이 넓혀주기
+        if diff < m:
+            right += 1
+        # 차이가 m보다 크거나 같으면 ans값 바꿔주고 차이 좁히기
+        elif diff > m:
+            ans = min(diff, ans)
             left += 1
-        elif (minus >= m) and (minus >= tmp):
-            left += 1
-        else: # m 이상이 아닌 경우, 차이가 더 커져야 함
-            if left == 0 and right < n-1:
-                right += 1
-            elif left > 0:
-                left -= 1
-            else:
-                break
-   
-    return tmp
+        # m과 차이가 동일하면 바로 리턴
+        else:
+            return diff
+    return ans
 
 
-ans = binary_search(number_list, m)
-print(ans)
+# left, right 모두 0부터 시작하여 오른쪽 한 방향으로만 이동
+print(two_pointer(0, 0))
+
+
+# m은 항상 0 이상이므로 left와 right의 값이 동일해지면 바로 차이를 넓혀주기 때문에
+# left가 right를 넘을 경우에 대한 조건을 따로 설정하지 않아도 된다
